563ff0dbd8d411be03775429c7f89aa4deb36078
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-06-24 19:32:44 -0300

    Adding loadDogList() on ViewModel layer

--- app/src/main/java/br/com/luisfernandez/dogclient/ui/doglist/DogListViewModel.kt
+++ app/src/main/java/br/com/luisfernandez/dogclient/ui/doglist/DogListViewModel.kt
@@ -1,7 +1,17 @@
 package br.com.luisfernandez.dogclient.ui.doglist
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import br.com.luisfernandez.dogclient.model.DogModelImpl
+import br.com.luisfernandez.dogclient.pojo.Dog
 
 class DogListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val dogListLiveData: MutableLiveData<List<Dog>> = MutableLiveData()
+
+    fun loadDogList(): LiveData<List<Dog>> {
+        dogListLiveData.value = DogModelImpl().loadDogList()
+        return dogListLiveData
+    }
 }
