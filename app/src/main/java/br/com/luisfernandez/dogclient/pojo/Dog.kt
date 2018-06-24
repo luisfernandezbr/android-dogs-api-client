f70062c4d41fa3b7c5e95ef2649a61b23f781b71
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-06-24 19:28:21 -0300

    Creating Dog pojo class

---
+++ app/src/main/java/br/com/luisfernandez/dogclient/pojo/Dog.kt
@@ -0,0 +1,28 @@
+package br.com.luisfernandez.dogclient.pojo
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Dog(
+        val name: String
+) : Parcelable {
+    constructor(parcel: Parcel) : this(parcel.readString())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Dog> {
+        override fun createFromParcel(parcel: Parcel): Dog {
+            return Dog(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Dog?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
