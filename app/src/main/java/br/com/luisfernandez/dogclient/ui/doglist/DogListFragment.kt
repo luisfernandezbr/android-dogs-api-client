cf3facf9cce5910be94b82f8ceb025f80e868298
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-06-24 20:00:25 -0300

    Adding RecyclerView and using Adapter to show dogs

--- app/src/main/java/br/com/luisfernandez/dogclient/ui/doglist/DogListFragment.kt
+++ app/src/main/java/br/com/luisfernandez/dogclient/ui/doglist/DogListFragment.kt
@@ -4,10 +4,13 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import br.com.luisfernandez.dogclient.DogListAdapter
 import br.com.luisfernandez.dogclient.R
 import br.com.luisfernandez.dogclient.pojo.Dog
 
@@ -18,10 +21,20 @@ class DogListFragment : Fragment() {
     }
 
     private lateinit var viewModel: DogListViewModel
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_dog_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_dog_list, container, false)
+
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById(R.id.recyclerView)
+
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = DogListAdapter()
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -29,15 +42,20 @@ class DogListFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(DogListViewModel::class.java)
 
         val observer = Observer<List<Dog>> { list: List<Dog>? ->
-
             list?.let {
                 for (dog in it) {
                     Log.d("", "DogName: ${dog.name}")
                 }
+                showContent(it)
             }
         }
 
         viewModel.loadDogList().observe(this, observer)
     }
 
+    fun showContent(dogList: List<Dog>) {
+        dogList.let {
+            (recyclerView.adapter as DogListAdapter).add(it)
+        }
+    }
 }
