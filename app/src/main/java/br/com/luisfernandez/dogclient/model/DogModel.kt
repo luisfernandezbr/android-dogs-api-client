02fcceafa50d6f920b127f4f6652acedd05cabe7
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-06-24 20:26:18 -0300

    Adding loadImageList() mock method

--- app/src/main/java/br/com/luisfernandez/dogclient/model/DogModel.kt
+++ app/src/main/java/br/com/luisfernandez/dogclient/model/DogModel.kt
@@ -1,7 +1,9 @@
 package br.com.luisfernandez.dogclient.model
 
 import br.com.luisfernandez.dogclient.pojo.Dog
+import br.com.luisfernandez.dogclient.pojo.DogImage
 
 interface DogModel {
     fun loadDogList(): List<Dog>
+    fun loadDogImageList(): List<DogImage>
 }
--- app/src/main/java/br/com/luisfernandez/dogclient/model/DogModelImpl.kt
+++ app/src/main/java/br/com/luisfernandez/dogclient/model/DogModelImpl.kt
@@ -1,15 +1,27 @@
 package br.com.luisfernandez.dogclient.model
 
 import br.com.luisfernandez.dogclient.pojo.Dog
+import br.com.luisfernandez.dogclient.pojo.DogImage
 
 class DogModelImpl : DogModel {
     override fun loadDogList(): List<Dog> {
-        var list = ArrayList<Dog>()
+        val list = ArrayList<Dog>()
 
         list.add(Dog("affenpinscher"))
         list.add(Dog("african"))
         list.add(Dog("airedale"))
         list.add(Dog("akita"))
+
+        return list
+    }
+
+    override fun loadDogImageList(): List<DogImage> {
+        val list = ArrayList<DogImage>()
+
+        list.add(DogImage("https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg"))
+        list.add(DogImage("https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg"))
+        list.add(DogImage("https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg"))
+        list.add(DogImage("https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg"))
 
         return list
     }
