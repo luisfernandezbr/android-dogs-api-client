3c6527c4816c42e87bf0be6c4eb4859021be713f
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-06-24 19:41:08 -0300

    Getting DogList on fragment

--- app/src/main/java/br/com/luisfernandez/dogclient/ui/doglist/DogListFragment.kt
+++ app/src/main/java/br/com/luisfernandez/dogclient/ui/doglist/DogListFragment.kt
@@ -1,12 +1,15 @@
 package br.com.luisfernandez.dogclient.ui.doglist
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.luisfernandez.dogclient.R
+import br.com.luisfernandez.dogclient.pojo.Dog
 
 class DogListFragment : Fragment() {
 
@@ -24,7 +27,17 @@ class DogListFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(DogListViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        val observer = Observer<List<Dog>> { list: List<Dog>? ->
+
+            list?.let {
+                for (dog in it) {
+                    Log.d("", "DogName: ${dog.name}")
+                }
+            }
+        }
+
+        viewModel.loadDogList().observe(this, observer)
     }
 
 }
